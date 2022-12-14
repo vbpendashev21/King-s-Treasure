e0e151791fab89e8a677395bfa76dff3a0fba76d
atpopov21 <atpopov21@codingburgas.bg>
2022-12-14 21:01:39 +0200

    Add pictures, improved password game

--- King's Treasure/King's Treasure/Functions.h
+++ King's Treasure/King's Treasure/Functions.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <conio.h>
 #include <string>
 using namespace std;
 
@@ -179,76 +180,76 @@ string wordDefinition(int wordNumber)
 {
 	switch (wordNumber)
 	{
-	case 1: return "Potato"; break;
-	case 2: return "Carrot"; break;
-	case 3: return "Bunny"; break;
-	case 4: return "Lorry"; break;
-	case 5: return "Carpet"; break;
-	case 6: return "Bike"; break;
-	case 7: return "Car"; break;
-	case 8: return "Truck"; break;
-	case 9: return "Desk"; break;
-	case 10: return "Football"; break;
-	case 11: return "Basketball"; break;
-	case 12: return "Volleyball"; break;
-	case 13: return "English"; break;
-	case 14: return "Chemistry"; break;
-	case 15: return "Biology"; break;
-	case 16: return "School"; break;
-	case 17: return "Airport"; break;
-	case 18: return "Bank"; break;
-	case 19: return "Chicken"; break;
-	case 20: return "Beef"; break;
-	case 21: return "Tradition"; break;
-	case 22: return "Ceremony"; break;
-	case 23: return "Headphones"; break;
-	case 24: return "Phone"; break;
-	case 25: return "Television"; break;
-	case 26: return "Social"; break;
-	case 27: return "Jeans"; break;
-	case 28: return "Programming"; break;
-	case 29: return "Window"; break;
-	case 30: return "Glass"; break;
-	case 31: return "Bedroom"; break;
-	case 32: return "Kitchen"; break;
-	case 33: return "Project"; break;
-	case 34: return "Balcony"; break;
-	case 35: return "Airfield"; break;
-	case 36: return "House"; break;
-	case 37: return "Home"; break;
-	case 38: return "Garage"; break;
-	case 39: return "Basement"; break;
-	case 40: return "Cellar"; break;
-	case 41: return "Museum"; break;
-	case 42: return "Clothes"; break;
-	case 43: return "Vocabulary"; break;
-	case 44: return "Grammar"; break;
-	case 45: return "People"; break;
-	case 46: return "Journey"; break;
-	case 47: return "Farmer"; break;
-	case 48: return "Engineer"; break;
-	case 49: return "Manager"; break;
-	case 50: return "Teacher"; break;
-	case 51: return "Banana"; break;
-	case 52: return "Apple"; break;
-	case 53: return "Orange"; break;
-	case 54: return "Kiwi"; break;
-	case 55: return "Payment"; break;
-	case 56: return "Money"; break;
-	case 57: return "Anger"; break;
-	case 58: return "Underweight"; break;
-	case 59: return "Overweight"; break;
-	case 60: return "Tall"; break;
-	case 61: return "Short"; break;
-	case 62: return "Towel"; break;
-	case 63: return "Head"; break;
-	case 64: return "Nose"; break;
-	case 65: return "Wrist"; break;
-	case 66: return "Foot"; break;
-	case 67: return "Headache"; break;
-	case 68: return "Trip"; break;
-	case 69: return "Jungle"; break;
-	case 70: return "Holiday"; break;
+		case 1: return "A round vegetable that grows underground and has white flesh with light brown."; break;
+		case 2: return "A long pointed orange root eaten as a vegetable."; break;
+		case 3: return "A small animal with long ears and large front teeth that moves by jumping on its long back legs."; break;
+		case 4: return "A large road vehicle that is used for transporting large amounts of goods."; break;
+		case 5: return "A layer of something that covers the ground. Mostly at your grandma's house."; break;
+		case 6: return "A two-wheeled vehicle that you sit on and move by turning the two pedals."; break;
+		case 7: return "A road vehicle with an engine, four wheels, and seats for a small number of people."; break;
+		case 8: return "A large road vehicle that is used for transporting large amounts of goods."; break;
+		case 9: return "A type of table that you can work at, often one with drawers."; break;
+		case 10: return "A game played between two teams of eleven people kicking a ball."; break;
+		case 11: return "A game played by two teams of five players who dribble and throw a ball."; break;
+		case 12: return "A game in which two teams use their hands to hit a ball over a high net."; break;
+		case 13: return "The language that is spoken in the UK, the US, and in many other countries."; break;
+		case 14: return "The scientific study of the basic characteristics of substances and the ways in which they react or combine."; break;
+		case 15: return "The scientific study of the natural processes of living things."; break;
+		case 16: return "A place where children go to be educated."; break;
+		case 17: return "A place where aircraft regularly take off and land."; break;
+		case 18: return "The building where people and businesses can invest or borrow money, change it to foreign money, etc."; break;
+		case 19: return "A type of bird kept on a farm for its eggs or its meat."; break;
+		case 20: return "The flesh of cows, eaten as food."; break;
+		case 21: return "A belief, principle, or way of acting that people in a particular society or group have continued to follow for a long time."; break;
+		case 22: return "Formal acts, often fixed and traditional, performed on important social or religious occasions"; break;
+		case 23: return "A device with a part to cover each ear through which you can listen to music"; break;
+		case 24: return "A device that is used to call people, play games, chat, etc."; break;
+		case 25: return "A system of broadcasting images and sound using electrical signals, and the programmes that are shown."; break;
+		case 26: return "Relating to activities in which you meet and spend time with other people."; break;
+		case 27: return "Trousers made of denim."; break;
+		case 28: return "The activity or job of writing computer programs."; break;
+		case 29: return "A large area of glass."; break;
+		case 30: return "A hard, transparent material, used to make windows, bottles, etc."; break;
+		case 31: return "A room used for sleeping in."; break;
+		case 32: return "A room where food is kept, prepared, and cooked and where the dishes are washed"; break;
+		case 33: return "A study of a particular subject done over a period of time, especially by students."; break;
+		case 34: return "An area with a fence around it that is connected to the exterior wall of an upper-level building."; break;
+		case 35: return "A level area where aircraft can take off and land."; break;
+		case 36: return "A building that people, usually one family, live in."; break;
+		case 37: return "The house, apartment, etc. where you live, especially with your family."; break;
+		case 38: return "A building where a car is kept, built next to or as part of a house."; break;
+		case 39: return "A part of a building that is below the level of the first floor."; break;
+		case 40: return "A room under the ground floor of a building, usually used for storing things."; break;
+		case 41: return "A building where objects of historical, scientific, or artistic interest are kept."; break;
+		case 42: return "Things you wear to cover your body and keep you warm, to be comfortable, etc."; break;
+		case 43: return "All the words that exist in a particular language or subject."; break;
+		case 44: return "The rules about how words change their form and combine with other words to make sentences."; break;
+		case 45: return "Used to refer to everyone (men, women and children)."; break;
+		case 46: return "The act of travelling from one place to another, especially in a vehicle."; break;
+		case 47: return "Someone who owns or takes care of a farm."; break;
+		case 48: return "A person whose job is to design or build machines, engines, or electrical equipment."; break;
+		case 49: return "The person who is responsible for managing an organization."; break;
+		case 50: return "Someone whose job is to teach in a school or college."; break;
+		case 51: return "A long, curved fruit with a yellow skin."; break;
+		case 52: return "A round fruit with firm, white flesh and a green, red, or yellow skin."; break;
+		case 53: return "A round sweet fruit that has a thick orange skin and an orange centre divided into many parts."; break;
+		case 54: return "An oval fruit with brown skin covered in hairs and bright green flesh."; break;
+		case 55: return "An amount of money that has been paid or is expected to be paid."; break;
+		case 56: return "The coins or bills with their value on them that are used to buy things."; break;
+		case 57: return "The feeling people get when something unfair, painful, or bad happens."; break;
+		case 58: return "Weighing less than usual."; break;
+		case 59: return "Heavier than is allowed."; break;
+		case 60: return "Above average height or of a certain height."; break;
+		case 61: return "Small in length, distance, or height."; break;
+		case 62: return "Usually rectangular piece of cloth or paper used for drying something."; break;
+		case 63: return "The part of the body above the neck where the eyes, nose, mouth, ears, and brain are."; break;
+		case 64: return "You can breathe and smell through it."; break;
+		case 65: return "The part of the body between the hand and the arm."; break;
+		case 66: return "The part of the body at the bottom of the leg on which a person or animal stands."; break;
+		case 67: return "A pain you feel inside your head."; break;
+		case 68: return "A journey in which you go somewhere, usually for a short time, and come back again."; break;
+		case 69: return "A tropical forest in which trees and plants grow very closely together."; break;
+		case 70: return "A time when someone does not go to work or school but is free to do what they want."; break;
 	}
 	return 0;
 }
@@ -277,7 +278,7 @@ void wordGame(int questionCounter, int randomNumber[6], bool answerKeepCheck[6],
 					}
 					cout << " |";
 				}
-				cout << endl << setw(362) << wordQuestionNumber(i);
+				cout << endl << setw(329) << wordQuestionNumber(i);
 				cout << wordDefinition(randomNumber[i]);
 				if (answerKeepCheck[i] == 1)
 				{
@@ -290,7 +291,7 @@ void wordGame(int questionCounter, int randomNumber[6], bool answerKeepCheck[6],
 			}
 			else
 			{
-				cout << endl << setw(362) << wordQuestionNumber(i);
+				cout << endl << setw(329) << wordQuestionNumber(i);
 				cout << wordDefinition(randomNumber[i]);
 				cout << endl << setw(357) << "Answer: ";
 			}
@@ -368,7 +369,7 @@ void wordGameAuto()
 	}
 }
 
-int password[6];
+int passwordKeep[6], password[6];
 
 void passwordGameGraphic() // make the graphic SGMikov21; change the function type from string to void
 {
@@ -419,7 +420,6 @@ int passwordSortSecondGameMode()
 int randomNumberGameMode()
 {
 	int randomNumberGameMode = 0;
-	srand(time(0));
 	randomNumberGameMode = (rand() % 2) + 1;
 	return randomNumberGameMode;
 }
@@ -427,19 +427,20 @@ int randomNumberGameMode()
 int randomNumbersPassword()
 {
 	int randomNumberPassword = 0;
-	srand(time(0));
 	randomNumberPassword = (rand() % 9) + 1;
 	return randomNumberPassword;
 }
 
 void passwordGame()
 {
-	int passwordUserInput, passwordSizeCheck = 0, counter = 0, userInputDowngrader = 10;
+	long long int passwordUserInput, passwordSizeCheck = 0;
+	int counter = 0, userInputDowngrader = 10;
 	bool passwordCheck = true;
 
 	passwordGameGraphic();
 	
 	cin >> passwordUserInput;
+	passwordSizeCheck = passwordUserInput;
 	do
 	{
 		passwordSizeCheck /= 10;
@@ -478,6 +479,7 @@ void passwordGameAuto()
 {
 	int gameMode = 0;
 	system("Color 0E");
+	srand(time(0));
 	for (int i = 0; i < 6; i++)
 	{
 		password[i] = randomNumbersPassword();
